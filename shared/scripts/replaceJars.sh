1aa6d16ae10836af3e8cc051af655926516d5aac
Paweł Wiejacha <pawel.wiejacha@gmail.com>
2015-02-06 23:02:34 +0100

    Added script for quick and dirty sbt/scalatest deployment

---
+++ shared/scripts/replaceJars.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# quick and dirty
+
+cp /shared/sbt/testing/target/testing-0.13.7-SNAPSHOT.jar ~/.sbt/boot/scala-2.10.4/org.scala-sbt/sbt/0.13.7-SNAPSHOT/testing-0.13.7-SNAPSHOT.jar
+cp /shared/sbt/main/command/target/command-0.13.7-SNAPSHOT.jar ~/.sbt/boot/scala-2.10.4/org.scala-sbt/sbt/0.13.7-SNAPSHOT/command-0.13.7-SNAPSHOT.jar
+cp /shared/scalatest/target/scala-2.11/scalatest_2.11-2.3.0-SNAPSHOT.jar ~/.ivy2/local/org.scalatest/scalatest_2.11/2.3.0-SNAPSHOT/bundles/scalatest_2.11.jar
