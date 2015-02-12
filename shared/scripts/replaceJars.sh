9addb024f566d99566e1e03d3f0e73b6c2390a3e
Paweł Wiejacha <pawel.wiejacha@gmail.com>
2015-02-12 20:46:30 +0100

    replacing sbt-logging jar in replaceJars.sh

--- shared/scripts/replaceJars.sh
+++ shared/scripts/replaceJars.sh
@@ -7,3 +7,5 @@ cp /shared/sbt/main/command/target/command-0.13.7-SNAPSHOT.jar ~/.sbt/boot/scala
 cp /shared/scalatest/target/scala-2.11/scalatest_2.11-2.3.0-SNAPSHOT.jar ~/.ivy2/local/org.scalatest/scalatest_2.11/2.3.0-SNAPSHOT/bundles/scalatest_2.11.jar
 cp /shared/sbt/main/actions/target/actions-0.13.7-SNAPSHOT.jar ~/.sbt/boot/scala-2.10.4/org.scala-sbt/sbt/0.13.7-SNAPSHOT/actions-0.13.7-SNAPSHOT.jar
 cp /shared/sbt/main/target/main-0.13.7-SNAPSHOT.jar ~/.sbt/boot/scala-2.10.4/org.scala-sbt/sbt/0.13.7-SNAPSHOT/main-0.13.7-SNAPSHOT.jar
+cp /shared/sbt/util/log/target/logging-0.13.7-SNAPSHOT.jar ~/.sbt/boot/scala-2.10.4/org.scala-sbt/sbt/0.13.7-SNAPSHOT/logging-0.13.7-SNAPSHOT.jar
+
